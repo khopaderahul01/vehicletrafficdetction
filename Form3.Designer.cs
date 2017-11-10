e9f6b07810a1626e2612ba784949bbd2a3a0439b
Rahul Khopade <khopaderahul01@gmail.com>
2017-11-10 11:09:05 +0530

    First commit

---
+++ Form3.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace VideoCaptureDemo
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape2 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbSourceDest = new System.Windows.Forms.ComboBox();
+            this.mtsDataSet = new VideoCaptureDemo.mtsDataSet();
+            this.mtsDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.mtsDataSet1 = new VideoCaptureDemo.mtsDataSet1();
+            this.sourceDestinationsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sourceDestinationsTableAdapter = new VideoCaptureDemo.mtsDataSet1TableAdapters.SourceDestinationsTableAdapter();
+            this.groupBox1.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mtsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtsDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtsDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sourceDestinationsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(89, 97);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(280, 54);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Capture From Camera",
+            "Capture From File"});
+            this.comboBox1.Location = new System.Drawing.Point(8, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.shapeContainer1);
+            this.groupBox5.Controls.Add(this.pictureBox3);
+            this.groupBox5.Location = new System.Drawing.Point(81, 170);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(4, 0, 4, 8);
+            this.groupBox5.Size = new System.Drawing.Size(296, 210);
+            this.groupBox5.TabIndex = 56;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Detection Preview";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(4, 13);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape2});
+            this.shapeContainer1.Size = new System.Drawing.Size(288, 189);
+            this.shapeContainer1.TabIndex = 1;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape2
+            // 
+            this.rectangleShape2.BorderColor = System.Drawing.Color.DarkRed;
+            this.rectangleShape2.Location = new System.Drawing.Point(220, -1);
+            this.rectangleShape2.Name = "rectangleShape1";
+            this.rectangleShape2.Size = new System.Drawing.Size(4, 176);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::VideoCaptureDemo.Properties.Resources.blank;
+            this.pictureBox3.Location = new System.Drawing.Point(8, 23);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(262, 173);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(413, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 30);
+            this.label1.TabIndex = 57;
+            this.label1.Text = "Total Vehicle Count: 0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbSourceDest);
+            this.groupBox2.Location = new System.Drawing.Point(97, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 58);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Source Destination";
+            // 
+            // cmbSourceDest
+            // 
+            this.cmbSourceDest.DataSource = this.sourceDestinationsBindingSource;
+            this.cmbSourceDest.FormattingEnabled = true;
+            this.cmbSourceDest.Location = new System.Drawing.Point(16, 19);
+            this.cmbSourceDest.Name = "cmbSourceDest";
+            this.cmbSourceDest.Size = new System.Drawing.Size(238, 21);
+            this.cmbSourceDest.TabIndex = 0;
+            this.cmbSourceDest.Text = "System.Data.DataViewManagerListItemTypeDescriptor";
+            // 
+            // mtsDataSet
+            // 
+            this.mtsDataSet.DataSetName = "mtsDataSet";
+            this.mtsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // mtsDataSetBindingSource
+            // 
+            this.mtsDataSetBindingSource.DataSource = this.mtsDataSet;
+            this.mtsDataSetBindingSource.Position = 0;
+            // 
+            // mtsDataSet1
+            // 
+            this.mtsDataSet1.DataSetName = "mtsDataSet1";
+            this.mtsDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sourceDestinationsBindingSource
+            // 
+            this.sourceDestinationsBindingSource.DataMember = "SourceDestinations";
+            this.sourceDestinationsBindingSource.DataSource = this.mtsDataSet1;
+            // 
+            // sourceDestinationsTableAdapter
+            // 
+            this.sourceDestinationsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 405);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mtsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtsDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtsDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sourceDestinationsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbSourceDest;
+        private System.Windows.Forms.BindingSource mtsDataSetBindingSource;
+        private mtsDataSet mtsDataSet;
+        private mtsDataSet1 mtsDataSet1;
+        private System.Windows.Forms.BindingSource sourceDestinationsBindingSource;
+        private mtsDataSet1TableAdapters.SourceDestinationsTableAdapter sourceDestinationsTableAdapter;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using System.Collections;
+using Emgu.CV.CvEnum;
+using Emgu.CV.VideoSurveillance;
+using System.IO;
+using System.Net;
+using System.IO.Ports;
+using System.Data.SqlClient;
+
+namespace VideoCaptureDemo
+{
+    public partial class Form3 : Form
+    {
+        private Capture _capture1 = null;       
+
+        Image<Bgr, Byte> frame;
+        
+        private IBGFGDetector<Bgr> _forgroundDetector;
+        
+        private static BlobTrackerAuto<Bgr> _tracker;
+       
+        private static MCvFont _font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_COMPLEX_SMALL, 1.0, 1.0);
+
+        ArrayList rectCnt = new ArrayList();
+      
+
+        ArrayList rlrectCnt = new ArrayList();
+       
+
+        MemStorage stor = new MemStorage();
+        
+        Image<Bgr, Byte> framecopy;
+       
+
+        Boolean oneCam = true;
+       
+        int cam = 0;
+        double webcam_frm_cnt = 0;
+        double FrameRate = 0;
+        double TotalFrames = 0;
+        double Framesno = 0;
+        
+        double codec_double = 0;
+        int one = 0;       
+       
+        string startupPath = Environment.CurrentDirectory;
+        SqlConnection Connection = new SqlConnection("");
+      
+        public Form3()
+        {
+            InitializeComponent();
+            SqlCommand cmd = new SqlCommand("", Connection);
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter dap = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dap.Fill(dt);
+            cmbSourceDest.DataSource = new BindingSource(dt, null);
+            cmbSourceDest.DisplayMember = "SourceDestination";
+            cmbSourceDest.ValueMember = "Id";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Select Capture Method");
+                button1.Enabled = true;
+            }
+            else
+                if (button1.Text == "Play")
+                {
+                    button1.Enabled = false;
+                    #region cameracapture
+                    if (comboBox1.Text == "Capture From Camera")
+                    {
+                        try
+                        {
+                            _capture1 = null;
+                            _capture1 = new Capture(0);
+                            _capture1.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS, 30);
+                            _capture1.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, 240);
+                            _capture1.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, 320);
+
+                            
+                            webcam_frm_cnt = 0;
+                            cam = 1;
+                            Application.Idle += ProcessFrame;
+                            button1.Text = "Stop";
+                            comboBox1.Enabled = false;
+                        }
+                        catch (NullReferenceException excpt)
+                        {
+                            MessageBox.Show(excpt.Message);
+                        }
+                    }
+                    #endregion cameracapture
+
+                    #region filecapture
+                    if (comboBox1.Text == "Capture From File")
+                    {
+
+                        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                        {
+                            try
+                            {
+                                _capture1 = null;
+                                String filepath = openFileDialog1.FileName;
+                                _capture1 = new Capture(filepath);
+                               
+                                _capture1.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, 240);
+                                _capture1.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, 320);
+
+                                
+
+                                FrameRate = _capture1.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS);
+                                TotalFrames = _capture1.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_COUNT);
+                                codec_double = _capture1.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FOURCC);
+                                string s = new string(System.Text.Encoding.UTF8.GetString(BitConverter.GetBytes(Convert.ToUInt32(codec_double))).ToCharArray());
+                                
+                                cam = 0;
+                                Application.Idle += ProcessFrame;
+                                button1.Text = "Stop";
+                                comboBox1.Enabled = false;
+                            }
+                            catch (NullReferenceException excpt)
+                            {
+                                MessageBox.Show(excpt.Message);
+                            }
+                            button1.Enabled = false;
+                        }
+                        else
+                        {
+                            button1.Enabled = true;
+                        }
+                    }
+                    #endregion filecapture
+
+                    
+                }
+                else
+                    #region stopcapture
+                    if (button1.Text == "Stop")
+                    {
+                        _capture1.Stop();
+                        button1.Enabled = true;
+                        Application.Idle -= ProcessFrame;
+
+                        ReleaseData();
+                        button1.Text = "Play";
+
+
+                        
+
+
+                        comboBox1.Enabled = true;
+                       
+                        pictureBox3.Image = Properties.Resources.blank;
+
+                       
+
+                        if (cam == 1)
+                        {
+                            _capture1.Dispose();
+                            cam = 0;
+                        }
+
+
+                        rectCnt.Clear();
+                        
+                        rlrectCnt.Clear();
+                       
+                       
+
+                    }
+                    #endregion stopcapture
+
+        }
+
+        /// <summary>
+        /// PROCESS EACH FRAME 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="arg"></param>
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+
+            try
+            {
+               
+                    frame = _capture1.QueryFrame();
+                    Framesno = _capture1.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_FRAMES);
+                
+
+                framecopy = frame.Rotate(180, new Bgr());
+               
+
+                #region framming
+
+                if (frame != null )
+                {
+                    
+
+                    if (_forgroundDetector == null)
+                    {
+                        _forgroundDetector = new FGDetector<Bgr>(Emgu.CV.CvEnum.FORGROUND_DETECTOR_TYPE.FGD_SIMPLE);
+                        //_forgroundDetector = new BGStatModel<Bgr>(frame, Emgu.CV.CvEnum.BG_STAT_TYPE.FGD_STAT_MODEL);
+                        _tracker = new BlobTrackerAuto<Bgr>();
+                    }
+
+                    
+                    _forgroundDetector.Update(frame);
+                    Image<Gray, Byte> todetectF = FillHoles(_forgroundDetector.ForegroundMask);
+
+                    
+
+                    Image<Gray, Byte> todetect = todetectF.Rotate(180, new Gray(255));
+                   
+
+                    _tracker.Process(todetect.Convert<Bgr, Byte>(), todetect);
+                    
+
+                    {
+                        Contour<Point> contours = todetect.FindContours(CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, RETR_TYPE.CV_RETR_LIST, stor);
+                        
+
+                        #region contours
+                        while (contours != null)
+                        {
+                            if (contours.Area > 200)// && (contours.BoundingRectangle.Width > 50 && contours.BoundingRectangle.Width < 100) && contours.BoundingRectangle.Height > 200)
+                            {
+                                Rectangle rect = contours.BoundingRectangle;
+                                rect.X = rect.X - 5;
+                                rect.Y = rect.Y - 5;
+                                rect.Height = (rect.Height + 10);
+                                rect.Width = (rect.Width + 10);
+                                Bitmap frame22 = framecopy.Bitmap;
+                                
+                                    framecopy.Draw(rect, new Bgr(Color.Red), 2);
+                               
+                                label1.Text = "Vehicle Count:" + (_tracker.Count);
+                               
+                                foreach (MCvBlob blob in _tracker)
+                                {
+                                    int blob_id = (blob.ID + 1);
+                                   // framecopy.Draw(blob_id.ToString(), ref _font, Point.Round(blob.Center), new Bgr(Color.Red));
+                                    
+                                    if (Point.Round(blob.Center).X > rectangleShape2.Location.X)
+                                    {
+                                        if (!rectCnt.Contains(blob_id))
+                                            rectCnt.Add(blob_id);
+
+                                    }
+
+
+                                }
+                                label1.Text = "Vehicle Count:" + rectCnt.Count;
+                                SqlCommand cmd = new SqlCommand("", Connection);
+                                cmd.CommandType = CommandType.Text;
+                                SqlDataAdapter dap = new SqlDataAdapter(cmd);
+                                DataTable dt = new DataTable();
+                                dap.Fill(dt);
+                                //MessageBox.Show(contours.Area.ToString());
+                            }
+                            contours = contours.HNext;
+                        }
+                        #endregion contours
+
+                       
+                        
+                        pictureBox3.Image = framecopy.ToBitmap();
+                       
+
+                    }
+                    if (cam == 0)
+                    {
+
+                        double time_index = _capture1.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_MSEC);
+                        //Time_Label.Text = "Time: " + TimeSpan.FromMilliseconds(time_index).ToString().Substring(0, 8);
+
+                        double framenumber = _capture1.GetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_POS_FRAMES);
+                        // Frame_lbl.Text = "Frame: " + framenumber.ToString();
+
+                    }
+
+                    if (cam == 1)
+                    {
+                        // Frame_lbl.Text = "Frame: " + (webcam_frm_cnt++).ToString();
+                    }
+                }
+                #endregion framming
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// FLOOD FILL IN MASK IMAGE
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+
+        private Image<Gray, byte> FillHoles(Image<Gray, byte> image)
+        {
+            var resultImage = image.CopyBlank();
+            Gray white = new Gray(255);
+            Gray black = new Gray(0);
+            using (var mem = new MemStorage())
+            {
+                for (var contour = image.FindContours(
+                    CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE,
+                    RETR_TYPE.CV_RETR_EXTERNAL,
+                    mem); contour != null; contour = contour.HNext)
+                {
+                    if (contour.Area > 200)
+                        resultImage.Draw(contour.BoundingRectangle, white, -1);
+                    else
+                        resultImage.Draw(contour.BoundingRectangle, black, -1);
+                }
+            }
+            return resultImage;
+        }
+
+        /// <summary>
+        /// RELEASE CAMERA/ FILE CAPTURE RESOURCE
+        /// </summary>
+        private void ReleaseData()
+        {
+            if (_capture1 != null)
+                _capture1.Dispose();
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'mtsDataSet1.SourceDestinations' table. You can move, or remove it, as needed.
+            this.sourceDestinationsTableAdapter.Fill(this.mtsDataSet1.SourceDestinations);
+
+        }
+
+    }
+}
